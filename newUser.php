3b4cc564d6a26a1745d7f94208bd18ed921bd9b5
Agata Rojek <agata85rojek@gmail.com>
2017-06-06 13:18:41 +0200

    Formularz do tworzenia nowego usera

--- newUser.php
+++ newUser.php
@@ -6,9 +6,9 @@
         <form action="index.php" method="POST">
             
             <input type="text" name="username" value="" placeholder="Name">
-            <input type="email" name="email" value="" placeholder="e-mail">
-            <input type="password" name="password" placeholder="password">
-            <button type="submit" >Sign In</button>
+            <input type="email" name="Email" value="" placeholder="e-mail">
+            <input type="password" name="Password" placeholder="password">
+            <button type="submit" name="signIn" >Sign In</button>
         </form>
         
     </body>
