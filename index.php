dd1747cfd48b86de46487cdc9b3a95867a18c3b3
Agata Rojek <agata85rojek@gmail.com>
2017-06-06 13:17:24 +0200

    Strona główna

--- index.php
+++ index.php
@@ -4,27 +4,68 @@
     
 
 
-    require_once 'User.php';
-
-    
-    
-    if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])){
-      $newUsername=$_POST['username'];
-      $newEmail=$_POST['email'];
-      $newPassword=$_POST['password'];
-    
-    $newUser = new User();
-    $newUser->setUsername($newUsername);
-    $newUser->setEmail($newEmail);
-    $newUser->setPassword($newPassword);
-    $newUser->saveToDB($mysqli);
-    }
+    require_once 'Class/User.php';
 
     
     include_once 'logIn.php';
     
-    if(isset($name)) echo $name.' Witaj na stronie głównej';
+    if(isset($_POST['username']) && isset($_POST['Email']) && isset($_POST['Password'])){
+      $newUsername=$_POST['username'];
+      $newEmail=$_POST['Email'];
+      $newPassword=$_POST['Password'];
+      
+      $sql = 'Select email FROM Users';
+      $result=$mysqli->query($sql);
+      if($result==true){
+          $row=$result->fetch_assoc();
+          if($row['email']==$newEmail){
+              echo '<font color="red">Podany e-mail już istnieje</font>';
+          }else{
+            $newUser = new User();
+            $newUser->setUsername($newUsername);
+            $newUser->setEmail($newEmail);
+            $newUser->setPassword($newPassword);
+            $newUser->saveToDB($mysqli);
+            
+             $sql="SELECT * FROM Users";
+             $result=$mysqli->query($sql);
+                 if($result==true && $result->num_rows>0){
+                     foreach ($result as $row){
+                      if($row['email']==$newEmail){
+                        $id = $row['id'];
+                        $name = $row['username']; 
+                      }
+                     }
+
+                 //var_dump($id);
+             }
+             
+   
+            $user=User::loadUserById($mysqli, $id);
+           // print_r($newUser);
+                return true;
+
+             }
+    
+     }
+    }
+    
+
+
+    
+    
+  
+      if (isset($user)){       
+           echo '<h3>'.$name.', witaj na stronie głównej!</h3>';
+           
+          include_once 'Class/Tweet.php';
+          include_once 'posts.php';//tutaj utknęłam!
+             //var_dump($date);
+              var_dump($id);
+
+      }
     
    // require_once 'foot.php';
-    
+    $mysqli->close();
+    $mysqli=null;
 ?>
