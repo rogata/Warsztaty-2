66e1f858dc253321cc8157d779435af03c5860a7
Agata Rojek <agata85rojek@gmail.com>
2017-06-09 01:21:34 +0200

    dodawanie do bazy nowych wpisów

--- posts.php
+++ posts.php
@@ -1,37 +1,38 @@
-<?php
-
- if(isset($_POST['post'])){
-            $newText =$_POST['post'];
-            //$date = 'NOW()';
-            $newPost = new Tweet();
-            $newPost->setText($newText);
-            $newPost->setCreationDate('NOW()');
-            $newPost->setUserId($id);
-            $newPost->saveToDB($mysqli);//nie zapisuje się do bazy
-            //jak klikam button w formularzu to strona ładuje się od nowa czyli pojawia się logowanie
-            return true;
-        }else echo 'nie ma wpisu';
-        
-        
-        $allPosts=Tweet::loadAllTweets($mysqli);
-        var_dump($allPosts);
-       // for($i=0; $i<count($allPosts); $i++){
-         //  echo $allPosts[$i];
-        //}
-        echo '<hr>';
-        
-
+<?php echo '<h3>'.$_SESSION['username'].', witaj na stronie głównej!</h3>'; 
+require_once 'Class/Tweet.php';
 ?>
-
 <!doctype html>
 <html>
     <body>
         <form action="" method="POST">
             <textarea cols="70" rows="10" name="post" placeholder="Dodaj wpis"></textarea>
-            <button type="submit">Opublikuj</button>
+            <button type="submit" name="btn">Opublikuj</button>
         </form>
     </body>
 </html>
+<?php
+$userId = $_SESSION['userId'];
+            // var_dump($userId);
 
+        if(isset($_POST['post'])){
+            $newText =$_POST['post'];
+            var_dump($newText);
+            var_dump($userId);
+           
+            $date = 'NOW()';
+            $newPost = new Tweet();
+            $newPost->setText($newText);
+            $newPost->setCreationDate($date);
+            $newPost->setUserId($userId);
+            $newPost->saveToDB($mysqli);
+            
+        }
+        
+        
 
+        
+        $allPosts=Tweet::loadAllTweets($mysqli);
+        var_dump($allPosts);
+  
+?>
 
