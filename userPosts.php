5e0860d29a06a8a8d78da1332492cac6e18deec3
Agata Rojek <agata85rojek@gmail.com>
2017-07-07 00:39:41 +0200

    create userPosts.php

---
+++ userPosts.php
@@ -0,0 +1,28 @@
+<?php
+
+    require_once 'config.php';
+    require_once 'db_conn.php';
+    require_once 'Class/User.php';
+    require_once 'Class/Tweet.php';
+    
+    if(isset($_GET['userId'])){
+        
+        $userId = $_GET['userId'];
+        $userPosts=Tweet::loadAllTweetsByUserId($mysqli, $userId);
+        $username=User::loadUserById($mysqli, $userId);
+       // var_dump($username);
+        $userName=$username->getUsername();
+        //var_dump($userPosts);
+      
+       echo '<table border="2" cellpadding="10">';
+        for($i=0; $i<count($userPosts); $i++){
+            echo '<tr>';
+            echo '<td>Nazwa użytkownika: '.$userName;//$userPosts[$i]->getUserId();
+            echo '<td>Tekst wpisu: '.$userPosts[$i]->getText()."</td>";
+            echo '<td>Data wpisu: '.$userPosts[$i]->getCreationDate()."</td>";
+            echo '</tr>';
+        }
+        echo '</table>';
+       
+    }
+        require_once 'foot.php';  
