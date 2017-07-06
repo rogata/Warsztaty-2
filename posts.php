3eb864afa93015e1377e04870f4b348274dba450
Agata Rojek <agata85rojek@gmail.com>
2017-07-07 00:38:56 +0200

    update

--- posts.php
+++ posts.php
@@ -1,5 +1,6 @@
-<?php echo '<h3>'.$_SESSION['username'].', witaj na stronie głównej!</h3>'; 
-require_once 'Class/Tweet.php';
+<?php 
+    echo '<h3>'.$_SESSION['username'].', witaj na stronie głównej!</h3>';
+    require_once 'Class/Tweet.php';
 ?>
 <!doctype html>
 <html>
@@ -15,18 +16,20 @@ require_once 'Class/Tweet.php';
     </body>
 <?php
 $userId = $_SESSION['userId'];
+               
+echo ' <a href="userPosts.php?userId='.$userId.'" style="float:left">Moje wpisy</a>';
             // var_dump($userId);
 $newText = "";
 
         if(isset($_POST['post'])){
             $newText =$_POST['post'];
-            var_dump($newText);
-            var_dump($userId);
+            //var_dump($newText);
+            //var_dump($userId);
             $sql = "SELECT text FROM Posts WHERE userId=$userId";
             $res = $mysqli->query($sql);
             if($res==true && $res->num_rows > 0){
                 if($row=$res->fetch_assoc()){
-                    if($row['text']!=$newText && $newText != ""){//po zapisaniu wartości $_POST['post'] cały czas dodawał się do bazy ten sap wpis po każdym odświrzeniu                                   //strony,dlatego postawiłam taki warunek, ale i tak czasami zapisze się dwa razy)
+                    if($row['text']!=$newText && $newText != ""){//po zapisaniu wartości $_POST['post'] cały czas dodawał się do bazy ten sam wpis po każdym odświrzeniu strony,dlatego postawiłam taki warunek, ale i tak czasami zapisze się dwa razy)
                         $date = 'NOW()';
                         $newPost = new Tweet();
                         $newPost->setText($newText);
@@ -47,30 +50,36 @@ $newText = "";
 
                 }
             }
-           
-            //  else if($newText != ""){    
-                   
                
         }
-        
-
         
         $allPosts=Tweet::loadAllTweets($mysqli);
         //var_dump($allPosts);
         
         
-        echo '<table border="2" cellpadding="10">';
         for($i=0; $i<count($allPosts); $i++){
+        echo '<table width="70%" height="10%">';
+            $sql = "SELECT username FROM Users WHERE id=".$allPosts[$i]->getUserId();
+            $result = $mysqli->query($sql);
+            if($result->num_rows>0){
+                foreach ($result as $row) {
+                $userName=$row['username'];
+                    
+                }
+            }
+            //var_dump($result);
             echo '<tr>';
-            echo '<td>'.$allPosts[$i]->getUserId();
+            echo '<td>'.$userName;
            // echo '<td colspan=2>'.$allPosts[$i]->getText()." ".$allPosts[$i]->getCreationDate()."</td>";
             echo '<td>'.$allPosts[$i]->getText()."</td>";
-            echo '<td>'.$allPosts[$i]->getCreationDate()."</td>";
-            echo '</tr>';
-        }
+            echo '<td style="float:right">'.$allPosts[$i]->getCreationDate()."</td>";
+            echo '</tr><hr width="70%">';
+            echo '<form action="" method="post">
+                    <textarea name="post" placeholder="Komentarz"></textarea>
+                    <button type="submit" name="btn">Dodaj</button>
+                    </form>';
         echo '</table>';
-    
-
+        }
         
 ?>
     </center>
