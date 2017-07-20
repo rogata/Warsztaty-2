224129424bbdc080434849d690821425bd32c6f2
Agata Rojek <agata85rojek@gmail.com>
2017-07-20 02:01:19 +0200

    login user SESSION

--- index.php
+++ index.php
@@ -1,64 +1,59 @@
 <?php
     session_start();
+    
     require_once 'config.php';
     require_once 'db_conn.php';
     
-    require_once 'Class/User.php';
+    require_once 'src/User.php';
+    require_once 'src/Tweet.php';
 
-    if(!empty($_SESSION['userId']) && !empty($_SESSION['username'])){
-    
-          include_once 'posts.php';
-          
-    }else{
-    
-    include_once 'logIn.php';
     
         
-    
-        if(isset($_POST['username']) && isset($_POST['Email']) && isset($_POST['Password'])){
-          $newUsername=$_POST['username'];
-          $newEmail=$_POST['Email'];
-          $newPassword=$_POST['Password'];
+            function registerNewUser($mysqli){
 
-          $sql = 'Select email FROM Users';
-          $result=$mysqli->query($sql);
-            if($result==true){
-              $row=$result->fetch_assoc();
-                if($row['email']==$newEmail){
-                    echo '<font color="red">Podany e-mail już istnieje</font>';
-                }else{
+                $newUserId = null;
 
-                $newUser = new User();
-                $newUser->setUsername($newUsername);
-                $newUser->setEmail($newEmail);
-                $newUser->setPassword($newPassword);
-                $newUser->saveToDB($mysqli);
+                if(isset($_POST['new_user']) && isset($_POST['new_email']) && isset($_POST['new_password'])){
+                  $newUsername=$_POST['new_user'];
+                  $newEmail=$_POST['new_email'];
+                  $newPassword=$_POST['new_password'];
 
-                 $sql="SELECT * FROM Users";
-                 $result=$mysqli->query($sql);
-                    if($result==true && $result->num_rows>0){
-                            foreach ($result as $row){
-                                if($row['email']==$newEmail){
-                                  $id = $row['id'];
-                                  $name = $row['username']; 
-                                    if($id > 0){
-                                        $_SESSION['userId'] = $id;
-                                        $_SESSION['username'] = $name;
-                                    }
+                  $sql = 'Select email FROM Users';
+                  $result=$mysqli->query($sql);
+                    if($result==true){
+                      $row=$result->fetch_assoc();
+                        if($row['email']==$newEmail){
+                            echo '<font color="red">Podany e-mail już istnieje</font>';
+                        }else{
 
-                                }
-                            }
+                        $newUser = new User();
+                        $newUser->setUsername($newUsername);
+                        $newUser->setEmail($newEmail);
+                        $newUser->setPassword($newPassword);
+                        $newUser->saveToDB($mysqli);
+
+                        $newUserId = $mysqli->insert_id;
+                        }
+
                     }
 
-                    
+                return $newUserId; 
                 }
 
             }
-
-        }
-         
+   // $_SESSION['userId']=registerNewUser($mysqli);  
+    if(!isset($_SESSION['userId'])){
+    
+        include_once 'logIn.php';
+            
+    }else{
+        include_once 'posts.php';
+    
     }
-          
+    
+
+        
+            
 if(isset($_POST['logOut']) && isset($_SESSION['userId'])){
     unset($_SESSION['userId']);
 }
@@ -66,4 +61,3 @@ if(isset($_POST['logOut']) && isset($_SESSION['userId'])){
     require_once 'foot.php';
     $mysqli->close();
     $mysqli=null;
-?>
