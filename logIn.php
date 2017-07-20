60b22155d130432a414d125089e573f7b0bde18e
Agata Rojek <agata85rojek@gmail.com>
2017-07-20 02:02:26 +0200

    add function login

--- logIn.php
+++ logIn.php
@@ -1,49 +1,45 @@
 <?php
-if(isset($_POST['email']) && isset($_POST['password'])){
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    
-    $sql="SELECT * FROM Users";
-    $result=$mysqli->query($sql);
-    if($result==true && $result->num_rows>0){
-        foreach ($result as $row){
-            if($row['email']==$email && password_verify($password, $row['hashed_password'])){
-               
-                $id = $row['id'];
-                $name = $row['username']; 
-                    if($id > 0){
-                        $_SESSION['userId'] = $id;
-                        $_SESSION['username'] = $name;
-                    }
-                
-                
-            $user=User::loadUserById($mysqli, $id);
-            
-              // return true;
-    
-            
-           // print_r($user);
+ 
+    function userLogin ($mysqli) {
         
+        $userId = 0;
+        
+        if(isset($_POST['email']) && isset($_POST['password']) && isset($_POST['logIn'])){
+            $email=$_POST['email'];
+            $password=$_POST['password'];
+             
+            $users = User::loadAllUsers($mysqli);
+            for($i=0; $i<count($users); $i++){
                 
-            }elseif($row['email']==$email && password_verify($password, $row['hashed_password'])==false) {
-                echo '<font color="red">Błędny login lub hasło!</font>';
-            }elseif($row['email']!=$email && password_verify($password, $row['hashed_password'])) {
-                echo '<font color="red">Błędny login lub hasło!</font>';
+                $userEmail = $users[$i]->getEmail();
+                $userPassword = $users[$i]->getPassword();
+            
+                if($userEmail==$email && password_verify($password, $userPassword)){
+
+                  $userId = $users[$i]->getId();
+                 
+
+                }elseif($userEmail==$email && password_verify($password, $userPassword)==false) {
+                    echo '<font color="red">Błędny login lub hasło!</font>';
+                }elseif($userEmail!=$email && password_verify($password, $userPassword)) {
+                    echo '<font color="red">Błędny login lub hasło!</font>';
+                }
+                
             }
+                return $userId;
         }
     }
-}
-
-
+var_dump(userLogin($mysqli));
+$_SESSION['userId'] = userLogin($mysqli); 
 ?>
 <!doctype html>
 <html>
     <body>
-        <h3>Zaloguj się</h3>
+        <h3>Twitter - logowanie</h3>
         <form action="" method="POST">
             <input type="email" name="email" value="" placeholder="e-mail">
             <input type="password" name="password" placeholder="password">
-            <button type="submit" >LogIn</button>
+            <button type="submit" name="logIn" >LogIn</button>
         </form>
         <h3>Jeśli nie masz jeszcze konta to zarejesrtuj się <a href="newUser.php"> TUTAJ!</a></h3>
     </body>
