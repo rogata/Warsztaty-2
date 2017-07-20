eb35588ca104b59d0c22c192806963645eff732e
Agata Rojek <agata85rojek@gmail.com>
2017-07-20 02:03:07 +0200

    add function register newUser

--- newUser.php
+++ newUser.php
@@ -5,9 +5,9 @@
         <h3>Zarejestruj się</h3>
         <form action="index.php" method="POST">
             
-            <input type="text" name="username" value="" placeholder="Name">
-            <input type="email" name="Email" value="" placeholder="e-mail">
-            <input type="password" name="Password" placeholder="password">
+            <input type="text" name="new_user" value="" placeholder="Name">
+            <input type="email" name="new_email" value="" placeholder="e-mail">
+            <input type="password" name="new_password" placeholder="password">
             <button type="submit" name="signIn" >Sign In</button>
         </form>
         
